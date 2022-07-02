4852df076810eba2793d855030da159342f398c3
Yi-Wei Chang <jchang6513@gmail.com>
2022-07-03 02:04:35 +0800

    Extract css module

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,6 +1,6 @@
 import { useCountdown } from '../hooks/useCountdown';
 import { Race } from '../types';
-import styles from '../../styles/Home.module.css'
+import styles from '../../styles/Countdown.module.css'
 
 export const Countdown = ({ nextRace }: { nextRace: Race }) => {
   const countdown = useCountdown(nextRace)
--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,7 +1,7 @@
+import { useState } from 'react';
 import { Race } from '../types';
 import { localTime } from '../utils';
-import styles from '../../styles/Home.module.css'
-import { useState } from 'react';
+import styles from '../../styles/Events.module.css'
 
 export const Events = ({ nextRace }: { nextRace: Race }) => {
   const [mobileGridToggle, setMobileGridToggle] = useState(false)
@@ -9,28 +9,28 @@ export const Events = ({ nextRace }: { nextRace: Race }) => {
   return (
     <>
       <div className={styles.actionBar} onClick={() => setMobileGridToggle(true)}>
-        <svg className={styles.arrow} viewbox="0 0 1792 1792">
+        <svg className={styles.arrow} viewBox="0 0 1792 1792">
           <path d="M1412 897q0-27-18-45l-91-91q-18-18-45-18t-45 18l-189 189v-502q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v502l-189-189q-19-19-45-19t-45 19l-91 91q-18 18-18 45t18 45l362 362 91 91q18 18 45 18t45-18l91-91 362-362q18-18 18-45zm252-1q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z" fill="#fff" />
         </svg>
       </div>
-      <div className={`${styles.grid} ${mobileGridToggle ? styles.active : ''}`}>
-        <div className={styles.card}>
+      <div className={`${styles.events} ${mobileGridToggle ? styles.active : ''}`}>
+        <div className={styles.event}>
           <h2>First Practice</h2>
           <p>{localTime(nextRace.FirstPractice)}</p>
         </div>
-        <div className={styles.card}>
+        <div className={styles.event}>
           <h2>Second Practice</h2>
           <p>{localTime(nextRace.SecondPractice)}</p>
         </div>
-        <div className={styles.card}>
+        <div className={styles.event}>
           <h2>Third Practice</h2>
           <p>{localTime(nextRace.ThirdPractice)}</p>
         </div>
-        <div className={styles.card}>
+        <div className={styles.event}>
           <h2>Qualifying</h2>
           <p>{localTime(nextRace.Qualifying)}</p>
         </div>
-        <div className={styles.card}>
+        <div className={styles.event}>
           <h2>Race</h2>
           <p>{localTime(nextRace)}</p>
         </div>
